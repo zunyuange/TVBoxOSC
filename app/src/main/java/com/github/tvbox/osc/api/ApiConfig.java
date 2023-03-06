31332e4e4dfb424cb99470f57fddeaad8f93c360
zunyuange <127128162+zunyuange@users.noreply.github.com>
2023-03-07 06:55:21 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://raw.githubusercontent.com/zunyuange/zunyaungeTV/main/meowcf.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
